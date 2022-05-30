bca30e9a71a06b31b39c6521e61f4db2558f6d75
Gonzalo Otero <gonzalo.chalo@gmail.com>
2022-05-30 15:45:11 +0200

    second exercise chapter 7

--- Introduction to computation and programming using python/Chapter 7/fingerExercise.py
+++ Introduction to computation and programming using python/Chapter 7/fingerExercise.py
@@ -13,3 +13,19 @@ def sumDigits(s):
         except ValueError:
             print("this character is not an integer")
     print(counter)
+
+
+
+# Implement a function that satisfies the specification
+
+def findAnEven(L):
+    """Assumes L is a list of integers
+    Returns the first even number in L
+    Raises valueError if L does not contain an even number"""
+
+    for x in L:
+        
+         if x % 2 == 0:
+            return x
+       
+    raise ValueError("no even integers")
