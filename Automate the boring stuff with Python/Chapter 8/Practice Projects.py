2f398ac5cd01aaf1be75058c9ca3a69b2bd74db8
Gonzalo Otero <gonzalo.chalo@gmail.com>
2022-07-24 21:51:57 +0100

    fixed formatting

--- Automate the boring stuff with Python/Chapter 8/Practice Projects.py
+++ Automate the boring stuff with Python/Chapter 8/Practice Projects.py
@@ -11,40 +11,40 @@ Using inputInt() to ask how many sandwiches they want. Make sure this number is 
 Come up with prices for each of these options, and have your program display a total cost after the user enters their selection.
 """
 
-# import pyinputplus as pyip
+import pyinputplus as pyip
 
-# menu = {"wheat": 2, "white": 3, "sourdough": 2, "chicken": 2, "ham": 3, "tofu": 4, "turkey": 1,
-#         "cheddar": 1, "swiss": 1, "mozarella": 1, "mayo": 1, "mustard": 1, "lettuce": 1, "tomato": 1, }
+menu = {"wheat": 2, "white": 3, "sourdough": 2, "chicken": 2, "ham": 3, "tofu": 4, "turkey": 1,
+        "cheddar": 1, "swiss": 1, "mozarella": 1, "mayo": 1, "mustard": 1, "lettuce": 1, "tomato": 1, }
 
-# order = []
+order = []
 
-# order.append(pyip.inputMenu(["wheat", "white", "sourdough"]))
+order.append(pyip.inputMenu(["wheat", "white", "sourdough"]))
 
-# order.append(pyip.inputMenu(["chicken", "turkey", "ham", "tofu"]))
+order.append(pyip.inputMenu(["chicken", "turkey", "ham", "tofu"]))
 
-# if pyip.inputYesNo("Do you want cheese with your order? ") == "yes":
+if pyip.inputYesNo("Do you want cheese with your order? ") == "yes":
 
-#     order.append(pyip.inputMenu(["cheddar", "swiss", "mozarella"]))
+    order.append(pyip.inputMenu(["cheddar", "swiss", "mozarella"]))
 
-# if pyip.inputYesNo("Do you want mustard with your order? ") == "yes":
-#     order.append("mustard")
+if pyip.inputYesNo("Do you want mustard with your order? ") == "yes":
+    order.append("mustard")
 
-# if pyip.inputYesNo("Do you want mayo with your order? ") == "yes":
-#     order.append("mayo")
+if pyip.inputYesNo("Do you want mayo with your order? ") == "yes":
+    order.append("mayo")
 
-# if pyip.inputYesNo("Do you want lettuce with your order? ") == "yes":
-#     order.append("lettuce")
+if pyip.inputYesNo("Do you want lettuce with your order? ") == "yes":
+    order.append("lettuce")
 
-# if pyip.inputYesNo("Do you want tomato with your order? ") == "yes":
-#     order.append("tomato")
+if pyip.inputYesNo("Do you want tomato with your order? ") == "yes":
+    order.append("tomato")
 
-# numSandwiches = pyip.inputInt("How many sandwiches do you want?", min=1)
+numSandwiches = pyip.inputInt("How many sandwiches do you want?", min=1)
 
-# total = 0
-# for menuItem in order:
-#     total += menu[menuItem]
+total = 0
+for menuItem in order:
+    total += menu[menuItem]
 
-# print(f'Total price: ${total * numSandwiches}')
+print(f'Total price: ${total * numSandwiches}')
 
 
 """
